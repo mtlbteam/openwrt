f98c5f7ad977330365e0215de56cc5a850ad21a7
Timur Valitov <vtima@MacBook-Air-Timur.local>
2025-06-23 19:42:49 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 ARCH=$(uname -m)
+RELEASE_ARCH=""
 
 case "$ARCH" in
   aarch64)
-    echo "arm64"
+    RELEASE_ARCH="arm64"
     ;;
   mips|mipsel)
-    echo "mips"
+    RELEASE_ARCH="mips"
     ;;
   *)
     echo "Unsupported architecture: $ARCH"
@@ -15,11 +16,29 @@ case "$ARCH" in
     ;;
 esac
 
+if ! command -v curl >/dev/null 2>&1; then
+  echo "curl not found, installing..."
+  opkg update && opkg install curl
+  if [ $? -ne 0 ]; then
+    echo "Failed to install curl!"
+    exit 1
+  fi
+fi
+
 if ! command -v wget >/dev/null 2>&1; then
   echo "wget not found, installing..."
   opkg update && opkg install wget
   if [ $? -ne 0 ]; then
     echo "Failed to install wget!"
+    exit 1
+  fi
+fi
+
+if ! command -v jq >/dev/null 2>&1; then
+  echo "jq not found, installing..."
+  opkg update && opkg install jq
+  if [ $? -ne 0 ]; then
+    echo "Failed to install jq!"
     exit 1
   fi
 fi
@@ -36,7 +55,27 @@ done
 echo "All required modules are installed"
 echo "Download bin"
 
-# todo: download
+ASSET_NAME="mtlb-${RELEASE_ARCH}"
+API_URL="https://api.github.com/repos/mtlbteam/openwrt/releases"
+
+RESPONSE=$(curl -s "$API_URL")
+
+DOWNLOAD_URL=$(echo "$RESPONSE" | jq -r \
+  '.[0].assets[]? | select(.name == "'"$ASSET_NAME"'") | .browser_download_url')
+
+if [ -z "$DOWNLOAD_URL" ]; then
+  echo "❌ Asset '$ASSET_NAME' not found in latest release."
+  exit 1
+fi
+
+echo $ASSET_NAME
+echo $DOWNLOAD_URL
+
+TARGET_PATH="/tmp/${ASSET_NAME}"
+
+curl -L -o "$TARGET_PATH" "$DOWNLOAD_URL"
+chmod +x "$ASSET_NAME"
+echo "✅ Downloaded $ASSET_NAME"
 
 if /etc/init.d/mtlb status | grep -q "running"; then
   echo "Stopping mtlb"
@@ -55,7 +94,7 @@ USE_PROCD=1
 
 start_service() {
   procd_open_instance
-  procd_set_param command /tmp/mtlb-bin
+  procd_set_param command ${$TARGET_PATH}
   procd_set_param stdout 1
   procd_set_param stderr 1
   procd_set_param respawn 5 5 10
