97af27a0d30ea43ab1b0994d57a9f766cb515231
Timur Valitov <vtima@MacBook-Air-Timur.local>
2025-06-23 19:22:07 +0200

    wip

--- install.sh
+++ install.sh
@@ -18,5 +18,39 @@ for module in $REQUIRED_MODULES; do
   fi
 done
 
-echo "All required modules are installed."
-echo "Download bin..."
+echo "All required modules are installed"
+echo "Download bin"
+
+# todo: download
+
+if /etc/init.d/mtlb status | grep -q "running"; then
+  echo "Stopping mtlb"
+  /etc/init.d/mtlb stop
+fi
+
+echo "Create service"
+
+cat << 'EOF' > /etc/init.d/mtlb
+#!/bin/sh /etc/rc.common
+
+START=99
+STOP=10
+
+USE_PROCD=1
+
+start_service() {
+  procd_open_instance
+  procd_set_param command /tmp/mtlb-bin
+  procd_set_param stdout 1
+  procd_set_param stderr 1
+  procd_set_param respawn 5 5 10
+  procd_set_param reload_signal HUP
+  procd_set_param term_timeout 60
+  procd_close_instance
+}
+EOF
+
+echo "Start service"
+
+chmod +x /etc/init.d/mtlb
+/etc/init.d/mtlb start
